688f7741b7c8dd4b136ca84f6fafaca2fefe32fd
InfinityCity18 <kubawlas1@gmail.com>
2024-08-04 21:26:09 +0200

    added channels

--- src/gateway/client.rs
+++ src/gateway/client.rs
@@ -1,11 +1,14 @@
 use crate::{error::BoxErr, API_VERSION};
-use errors::{EmptyEventDataError, MessageError, NotHelloError};
+use errors::{EmptyEventDataError, MessageError, NoHeartbeatACKError, NotHelloError};
+use futures_util::stream::{SplitSink, SplitStream};
 use futures_util::{SinkExt, StreamExt};
-use reqwest::Url;
+use reqwest::{get, Url};
 use std::error::Error;
 use std::ops::Not;
 use std::time::Duration;
 use tokio::net::TcpStream;
+use tokio::sync::mpsc;
+use tokio::sync::Mutex;
 use tokio_tungstenite::tungstenite::WebSocket;
 use tokio_tungstenite::{connect_async, tungstenite::Message};
 use tokio_tungstenite::{MaybeTlsStream, WebSocketStream};
@@ -15,7 +18,8 @@ use super::event::{EventData, GatewayEvent, OpCode};
 use crate::api::ApiClient;
 use std::sync::Arc;
 
-type WsStream = WebSocketStream<MaybeTlsStream<TcpStream>>;
+type WsTx = Arc<Mutex<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>>;
+type WsRx = Arc<Mutex<SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>>>;
 type BoxError = Box<dyn std::error::Error + Send + Sync>;
 
 const JITTER: u64 = 2;
@@ -34,8 +38,14 @@ impl GatewayClient {
         gateway_url.set_query(Some(format!("v={}", API_VERSION).as_str()));
         gateway_url.set_query(Some("encoding=json"));
 
-        let (mut wsstream, _) = connect_async(gateway_url.as_str()).await.bx()?;
-        let init_msg = wsstream.next().await.unwrap().bx()?;
+        let (wsstream, _) = connect_async(gateway_url.as_str()).await.bx()?;
+        let (wstx, wsrx) = wsstream.split();
+        let (wstx, wsrx) = (Arc::new(Mutex::new(wstx)), Arc::new(Mutex::new(wsrx)));
+
+        let (sender, mut receiver) = mpsc::unbounded_channel::<Message>();
+
+        get_msg(sender.clone(), wsrx.clone()).await?;
+        let init_msg = receiver.recv().await.unwrap();
         let init_msg: GatewayEvent = init_msg.try_into()?;
 
         if init_msg.op != OpCode::Hello {
@@ -54,9 +64,13 @@ impl GatewayClient {
 
         tokio::time::sleep(Duration::from_millis(hb_interval / JITTER)).await;
 
-        print_type_of(&wsstream);
+        send_msg(first_hb, wstx).await?;
+        get_msg(sender.clone(), wsrx.clone()).await?;
 
-        wsstream.send(first_hb).await.bx()?;
+        let ack: GatewayEvent = receiver.recv().await.unwrap().try_into()?;
+        if OpCode::HeartbeatACK != ack.op {
+            return Err(NoHeartbeatACKError).bx()?;
+        }
 
         Ok(GatewayClient { gateway_url })
     }
@@ -67,8 +81,18 @@ fn print_type_of<T>(_: &T) {
 }
 
 async fn supervisor() {}
-async fn send_msg(msg: Message, stream: WsStream) -> Result<(), BoxError> {
-    Ok(stream.send(msg).await?)
+
+async fn send_msg(msg: Message, tx: WsTx) -> Result<(), BoxError> {
+    let mut lock = tx.lock().await;
+    lock.send(msg).await.bx()?;
+    Ok(())
+}
+
+async fn get_msg(sender: mpsc::UnboundedSender<Message>, rx: WsRx) -> Result<(), BoxError> {
+    let mut lock = rx.lock().await;
+    let msg = lock.next().await.unwrap()?;
+    sender.send(msg)?;
+    Ok(())
 }
 
 mod errors {
